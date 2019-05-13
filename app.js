d831c1117d460b4ad87992fd1dffbbf00da8c111
Gagandeep Singh <gagan.ddun@gmail.com>
2019-05-13 22:33:26 +1000

    added new route for posts request

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv/config');
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
 
 app.get('/', (req, res) => {
     res.send('Hello World !!');
