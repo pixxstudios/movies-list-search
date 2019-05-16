cdc33482b551da2cb7f12c09afc7c31036c5030d
Gagandeep Singh <gagan.ddun@gmail.com>
2019-05-16 22:07:45 +1000

    added new components

---
+++ src/js/book-card/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default () => {
+    return (
+        <div>
+            "This is a book card"
+        </div>
+    )
+}
---
+++ src/js/home/home.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export default () => {
+    <h3>Home Page</h3>
+}
