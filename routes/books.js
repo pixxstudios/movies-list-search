c1debbe2acae8afe1ddfe02a54f716f6c4416d27
Gagandeep Singh <gagan.ddun@gmail.com>
2019-05-17 19:45:23 +1000

    api update to return all books

--- routes/books.js
+++ routes/books.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const Book = require('../models/book');
 
-router.get('/', (req, res) => {
-    res.send('We are inside books');
+router.get('/',async (req, res) => {
+    const books = await Book.find();
+    res.json(books);
 });
 
 router.post('/', async (req, res) => {
