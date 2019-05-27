68050e87ac65d4738adc2a95fcf2f859ac9e2647
Gagandeep Singh <gagan.ddun@gmail.com>
2019-05-27 19:42:36 +1000

    moved store to a separate file

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import {render} from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 import BooksApp from './components/books-app/index';
-import reducers from './reducers';
+// import reducers from './reducers';
+
+import store from './store';
 
 render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
         <BooksApp />
     </Provider>
     ,
