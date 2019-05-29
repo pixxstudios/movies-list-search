c1d55d532fe4150d97eeeb0d0b41bb8e1542e056
Gagandeep Singh <gagan.ddun@gmail.com>
2019-05-29 21:27:04 +1000

    deleted files

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,5 @@
-const GET_ALL_BOOKS = 'GET_ALL_BOOKS';
-const SAVE_BOOK = 'SAVE_BOOK';
+export const GET_ALL_BOOKS = 'GET_ALL_BOOKS';
+export const SAVE_BOOK = 'SAVE_BOOK';
 
 export const getAllBooks = () => ({ 'type': GET_ALL_BOOKS });
 export const saveBook = () => ({ 'type': SAVE_BOOK });
--- src/components/home/actions.ts
+++
@@ -1,3 +0,0 @@
-const GET_ALL_BOOKS = 'GET_ALL_BOOKS';
-
-export const getAllBooks = () => ({ 'type': GET_ALL_BOOKS });
