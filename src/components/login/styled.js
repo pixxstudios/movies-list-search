7bccb23a547479d17596bfe53570a70b2ae1b376
Gagandeep Singh <gagan.ddun@gmail.com>
2019-05-30 22:07:24 +1000

    updated css

--- src/components/login/styled.js
+++ src/components/login/styled.js
@@ -4,6 +4,9 @@ const LoginWrapper = styled.div`
     display: flex;
     width: 100vw;
     height: 100vh;
+    background-color: #478dff;
+    color: white;
+    padding: 30px;
 `;
 
 export default LoginWrapper;
