68533184753bcd08f30e31cbc25ffd2adabc3181
Gagandeep Singh <gagan.ddun@gmail.com>
2019-05-30 20:02:41 +1000

    updated container

--- src/components/books-app/index.tsx
+++ src/components/books-app/index.tsx
@@ -1,15 +1,16 @@
 import * as React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Container from '@material-ui/core/Container';
 import Header from '../header';
 import Home from '../home'
 
 export default () => {
 return(
-<div className="container-fluid">
-    <BrowserRouter>
-        <Header />
-        <Home />
-    </BrowserRouter>
-</div>
-)
+        <Container>
+            <BrowserRouter>
+                <Header />
+                <Home />
+            </BrowserRouter>
+        </Container>
+    )
 }
