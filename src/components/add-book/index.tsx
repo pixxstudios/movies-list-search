cdd86f8e2324fd1d9e33db6d0e665bf0835d99bf
Gagandeep Singh <gagan.ddun@gmail.com>
2019-06-11 19:54:17 +1000

    updated button styling

--- src/components/add-book/index.tsx
+++ src/components/add-book/index.tsx
@@ -11,8 +11,10 @@ const AddBook = () => {
             <AddBookWrapper>
                 <input type='text' id='title' placeholder='Book title ...'/>
                 <img src='https://media1.popsugar-assets.com/files/thumbor/6j4QtP0kTb0cNF9A7vvMG2lWzbc/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2019/03/18/739/n/1922283/c460649f5c8fcb249041a7.72354065_/i/JK-Rowling-Backlash-LGBTQ-Inclusion-Comments.jpg' />
-                <Button variant={PRIMARY} value="Add" />
-                <Button variant={PRIMARY} value="Cancel" />
+                <div>
+                    <Button variant={PRIMARY} value="Add" />
+                    <Button variant={PRIMARY} value="Cancel" />
+                </div>
             </AddBookWrapper>
         </div>
     );
