d231e0e11a18615589a10a490e784de9493512e1
Gagandeep Singh <gagan.ddun@gmail.com>
2019-06-11 19:47:05 +1000

    fixed bugs

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import ButtonWrapper from './styled';
+import { PRIMARY } from './variants';
 
 const Button = props => {
     return (
@@ -11,11 +12,15 @@ const Button = props => {
 };
 
 Button.defaultProps = {
-    click: () => {}
+    click: () => {},
+    variant: PRIMARY,
+    value: ''
 };
 
 Button.propTypes = {
-    click: PropTypes.func
+    click: PropTypes.func,
+    variant: PropTypes.string,
+    value: PropTypes.string
 };
 
 export default Button;
