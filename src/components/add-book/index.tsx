0477a5850d3b3e289473757807da15953cf829ec
Gagandeep Singh <gagan.ddun@gmail.com>
2019-06-12 19:16:01 +1000

    updated ui

--- src/components/add-book/index.tsx
+++ src/components/add-book/index.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
 import AddBookWrapper from './styled';
 import Header from '../header';
-import { Button } from 'antd';
+import { Button, Input, Rate } from 'antd';
 // import { PRIMARY } from '../button/variants';
-import Multiselect from 'multiselect-dropdown-react';
 
 const AddBook = props => {
     const image = null;
@@ -47,11 +46,12 @@ const AddBook = props => {
             <Header />
             <AddBookWrapper>
                 <img src='https://media1.popsugar-assets.com/files/thumbor/6j4QtP0kTb0cNF9A7vvMG2lWzbc/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2019/03/18/739/n/1922283/c460649f5c8fcb249041a7.72354065_/i/JK-Rowling-Backlash-LGBTQ-Inclusion-Comments.jpg' />
-                <input type='text' id='title' placeholder='Book title ...'/>
-                <input type='author' id='author' placeholder='Book author ...'/>
-                <Multiselect options={categories} onSelectOptions={onCategoriesSelect}/>
+                <Input placeholder='Book title' allowClear/>
+                <Input placeholder='Book author' allowClear onChange={onCancel} />
+                <Rate />
                 <div>
                     <Button type="primary">Primary</Button>
+                    <Button type="danger">Cancel</Button>
                 </div>
             </AddBookWrapper>
         </div>
