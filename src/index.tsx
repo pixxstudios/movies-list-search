3c90a8aac1b0f864c8777efd7d43e0cb4ce8df0f
Gagandeep Singh <gagan.ddun@gmail.com>
2019-07-27 17:37:55 +1000

    removed package lock file

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {render} from 'react-dom';
 import { Provider } from 'react-redux';
-import BooksApp from './components/books-app/index';
+import BooksApp from './components/movies-app/index';
 import store from './store';
 
 render(
