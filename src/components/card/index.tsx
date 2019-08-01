06fe395a3d95cf3d1fb8966c6a124e92be4c02a9
Gagandeep Singh <gagan.ddun@gmail.com>
2019-08-01 20:55:35 +1000

    bug fixing

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -3,15 +3,15 @@ import { v4 } from 'uuid';
 import './style.scss';
 
 const Card = props => {
-    const { data }= props;
+    const { data } = props;
     
     return(
-        <div className="card" key={ v4() }>
+        <div className="card" key={v4()}>
             <div>
                 <h3>{data.title} ({data.year})</h3>
                 <p>{data.storyline}</p>
                 <h4>IMDB Rating: {data.imdbRating}</h4>
-                { data.actors && data.actors.map((actor, i) => <span className="actor" key={`actor${i}`}>{actor}</span> )}
+                { data.actors && data.actors.map((actor, i) => <span className="actor" key={v4()}>{actor}</span> )}
             </div>
             <img src={data.posterurl} />
         </div>
